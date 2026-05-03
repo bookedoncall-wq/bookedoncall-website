601c8da989a222c61de02e7ee6dc41a96efe704c
David Carley <davidrcarley@gmail.com>
2026-05-03 02:06:23 -0600

    Add Next server type shim

---
+++ next-server-js.d.ts
@@ -0,0 +1,9 @@
+declare module "next/server" {
+  export { NextRequest } from "next/dist/server/web/spec-extension/request"
+  export { NextResponse } from "next/dist/server/web/spec-extension/response"
+}
+
+declare module "next/server.js" {
+  export { NextRequest } from "next/dist/server/web/spec-extension/request"
+  export { NextResponse } from "next/dist/server/web/spec-extension/response"
+}
